b7821ab4577309061933d4135a5923bcab7c9ea0
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-06 18:50:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   binding.pry
-  new_hash = holiday_hash.collect{ |key,value| [key.to_s, value.to_s]}
+  new_hash = []
+  holiday_hash.collect do |key, value|
+    key.to_s, value.to_s
 
     #holiday_hash.each do |key, value|
     #  key.to_s
