f315006d466cff4b13ec6afe7bd3cb57da92c20f
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-06 21:09:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_supplies_in_holidays(holiday_hash)
       holiday_hash.map do |season, holiday|
       puts "#{season.to_s.capitalize!}:"
         season.map do |holiday, data|
-          puts "#{holiday.to_s.captitalize!}:""
+          puts "#{holiday.to_s.captitalize!}:"
 
     end
   end
